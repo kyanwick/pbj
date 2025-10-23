15a29ca02e22593a4d820d5b2ecc8643517a9934
kyanwick <kyanmigueloberas1@gmail.com>
2025-10-23 09:52:12 +0000

    fixed connection locally

--- quasar.config.js
+++ quasar.config.js
@@ -74,6 +74,13 @@ export default defineConfig((/* ctx */) => {
     devServer: {
       // https: true,
       open: true, // opens browser window automatically
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+          ws: true
+        }
+      }
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
